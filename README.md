3b64dfc0deb80ac0a21b2793b2b4b3530ae4a88e
Axel Rock <rockaxel@gmail.com>
2023-01-23 09:33:23 +0100

    Project setup

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# bang
-Bannerboy Ad Naming Guidelines
+# Bannerboy Ad Naming Guidelines
+
+## Why?
+
+These guidelines are meant to make it easier for Bannerboy internal tools to communicate, by sharing the same naming convention.
+
+For this first draft, we will often compare to what was previously called "Unit" in several tools.
+
+Everything written here is open to debate.
+
+## Creative vs Unit
+
+For years, we've called things "Units".
+
+## Width and Height vs dimensions
+
+Dimensions as a string ie: `'300x250'` have been the standard. But almost every tool requires to parse the width and height. So there are constant conversions between that string, to these 2 values. This also increases error risks, as it's easy to forget to cast the parsed width and height to numbers when splitting the string.
+
+To avoid this, it's recommended to use width and height as numbers.
+
+For a fully responsive banner, it's recommended to use width and height of 1.
+
+Because dimensions written that way is an industry standard, there are getters and setters. Which means you can still use them as previously. But it's no longer the main and recommended way.
+
+## Size vs FileSize vs Weight
+
+Size could be mistaken from dimensions. So, weight was proposed instead.
+
+But, weight is not a standard is programming. Most tools and libraries, especially in JavaScript, use the word size, or filesize. I suggest sticking to this to match the standards.
+
+Also, file size should be set in bytes. You're free to display them in any other units in your application. But this once again avoids parsing, and parsing errors. It also limits a bit of the hassle of choosing between kb and kib (1000 bytes, or 1024 bytes)
+
+Quick tip: to make it easier to read in JavaScript, it's possible to use natively an underscore to seperate numbers wherever you want:
+
+100000 > 100_000
---
+++ unit.ts
@@ -0,0 +1,75 @@
+class Creative {
+  /**
+   * id of the creative is optional. It has to be unique, and is mainly use for database
+   */
+  id?: string;
+
+  /**
+   * name of the creative. This must be unique within a campaign
+   */
+  name: string;
+
+  /**
+   * type of the creative. This doesn't go into detail.
+   * It can be used to determine which kind of html tag is needed to display the creative
+   */
+  type: CreativeType; 
+
+  /**
+   * format of the creative. This gived more detail about the type of the creative.
+   * It can be used to apply export settings
+   * This is optional, because it can be determined by the type in some cases
+   */
+  format?: CreativeFormat;
+
+  /**
+   * Default width in px. Use 1 for responsive in width
+   */ 
+  width: number;
+
+  /**
+   * Default width in px. Use 1 for responsive in height
+   */ 
+  height: number;
+
+  /**
+   * The maximum file size in bytes. This is used to determine if the creative is too big to be uploaded.
+   */
+  maxFileSize?: number;
+
+  /**
+   * The current file size in bytes. 
+   */
+  fileSize: number;
+
+  get dimensions(): string {
+    return [this.width, this.height].join(Creative.DIMENSIONS_SEPARATOR);
+  }
+
+  set dimensions(dimensions: string) {
+    const [width, height] = dimensions.split(Creative.DIMENSIONS_SEPARATOR);
+    this.width = parseInt(width, 10);
+    this.height = parseInt(height, 10);
+  }
+
+  /**
+   * Define which character is used to separate the width and height in a dimensions string.
+   * At Bannerboy, we've been using "x", which is easy to type.
+   * However, the multiplication sign "×" would be more accurate.
+   */
+  static DIMENSIONS_SEPARATOR = 'x';
+  // static DIMENSIONS_SEPARATOR = '×';
+}
+
+class HTMLCreative extends Creative {
+  clickTag: string;
+
+  /**
+   * Previously known as "platform"
+   */
+  adserver: string;
+}
+
+type CreativeType = 'html' | 'image' | 'video' | 'photoshop';
+
+type CreativeFormat = 'html' | 'jpg' | 'png' | 'gif' | 'mp4' | 'mov' | 'psd';
